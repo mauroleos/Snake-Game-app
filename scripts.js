0d65b5cd2c72446b3fb01c41906423dd21ab94eb
mauro Leos <mauroleos94@gmail.com>
2020-06-08 08:42:28 -0500

    Worked on JS

--- scripts.js
+++ scripts.js
@@ -1,20 +1,28 @@
 var canvas = document.getElementById('game-canvas');
 var canvasContext = canvas.getContext('2d');
-snake = 50;
-apple = 500;
+var snakePositionX = 150;
+var snakePositionY = 150;
+var apple = 500;
+var snakeSpeed = 5;
 
+//BACKGROUND//
 canvasContext.beginPath();
 canvasContext.rect(0, 0, 800, 600);
 canvasContext.fillStyle = 'black';
 canvasContext.fill();
-
+//SNAKE//
 canvasContext.beginPath();
-canvasContext.rect(snake, 100, 25, 25);
+canvasContext.rect(snakePositionX, snakePositionY, 25, 25);
 canvasContext.fillStyle = 'yellow';
 canvasContext.fill();
-
+//APPLE//
 canvasContext.beginPath();
 canvasContext.rect(apple, 400, 25, 25);
 canvasContext.fillStyle = 'red';
 canvasContext.fill();
 
+canvas.addEventListener('keydown', function (e){
+    if(e.keycode = 39){
+        alert('it works')
+    }
+})
