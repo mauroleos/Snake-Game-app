5695a5062242e746a11fcb026de7d77046176fb7
mauro Leos <mauroleos94@gmail.com>
2020-06-10 09:00:38 -0500

    added switch statement

--- scripts.js
+++ scripts.js
@@ -1,5 +1,34 @@
+window.onload = function() {
 var canvas = document.getElementById('game-canvas');
 var canvasContext = canvas.getContext('2d');
+document.addEventListener('keydown', 'keyPush');
+setInterval(game, 1000/15); //game is a function
+}
+
+snakeVelocityX = 0;
+snakeVelocityY = 0; 
+
+function keyPush(e){
+    switch(e.keyCode) {
+        case 37 : 
+        snakeVelocityX = -1;
+        snakeVelocityY = 0;
+        break;
+        case 38 : 
+        snakeVelocityX = 0;
+        snakeVelocityY = -1;
+        break;
+        case 39 : 
+        snakeVelocityX = 1;
+        snakeVelocityY = 0;
+        break;
+        case 40 : 
+        snakeVelocityX = 0;
+        snakeVelocityY = 1;
+        break;
+    }
+}
+
 var snakePositionX = 150;
 var snakePositionY = 150;
 var apple = 500;
@@ -21,8 +50,9 @@ canvasContext.rect(apple, 400, 25, 25);
 canvasContext.fillStyle = 'red';
 canvasContext.fill();
 
-canvas.addEventListener('keydown', function (e){
-    if(e.keycode = 39){
-        snakePositionY += 5;
+function move(e){
+    if(e.keycode = 38){
+        snakePositionY += -5;
     }
-})
+    if(e.keyCode = 40){
+        snakePositionY += 5;
