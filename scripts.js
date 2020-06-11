6797841eb2cc00b16e57317d430b95c568daa1ad
mauro Leos <mauroleos94@gmail.com>
2020-06-10 22:38:16 -0500

    applied key codes to make the snake move

--- scripts.js
+++ scripts.js
@@ -1,58 +1,52 @@
-window.onload = function() {
-var canvas = document.getElementById('game-canvas');
-var canvasContext = canvas.getContext('2d');
-document.addEventListener('keydown', 'keyPush');
-setInterval(game, 1000/15); //game is a function
-}
+canvas = document.getElementById('game-canvas');
+canvasContext = canvas.getContext('2d');
+document.addEventListener('keydown', keyPush);
+setInterval(game, 1000/15);
 
-snakeVelocityX = 0;
-snakeVelocityY = 0; 
+var snakeVelocityX = 0;
+var snakeVelocityY = 0; 
+var snakePositionX = 150;
+var snakePositionY = 150;
+var appleX = 500;
+var appleY = 400;
+
+function game() {
+    snakePositionX += snakeVelocityX;
+    snakePositionY += snakeVelocityY;
+    
+    canvasContext.beginPath();
+    canvasContext.fillStyle = 'black';
+    canvasContext.rect(0, 0, canvas.width, canvas.height);
+    canvasContext.fill();
+
+    canvasContext.beginPath();
+    canvasContext.fillStyle = 'yellow';
+    canvasContext.rect(snakePositionX, snakePositionY, 25, 25);
+    canvasContext.fill();
+
+    canvasContext.beginPath();
+    canvasContext.fillStyle = 'red';
+    canvasContext.rect(appleX, appleY, 25, 25);
+    canvasContext.fill();
+}
 
 function keyPush(e){
     switch(e.keyCode) {
         case 37 : 
-        snakeVelocityX = -1;
+        snakeVelocityX = -5;
         snakeVelocityY = 0;
         break;
         case 38 : 
         snakeVelocityX = 0;
-        snakeVelocityY = -1;
+        snakeVelocityY = -5;
         break;
         case 39 : 
-        snakeVelocityX = 1;
+        snakeVelocityX = 5;
         snakeVelocityY = 0;
         break;
         case 40 : 
         snakeVelocityX = 0;
-        snakeVelocityY = 1;
+        snakeVelocityY = 5;
         break;
     }
 }
-
-var snakePositionX = 150;
-var snakePositionY = 150;
-var apple = 500;
-var snakeSpeed = 5;
-
-//BACKGROUND//
-canvasContext.beginPath();
-canvasContext.rect(0, 0, 800, 600);
-canvasContext.fillStyle = 'black';
-canvasContext.fill();
-//SNAKE//
-canvasContext.beginPath();
-canvasContext.rect(snakePositionX, snakePositionY, 25, 25);
-canvasContext.fillStyle = 'yellow';
-canvasContext.fill();
-//APPLE//
-canvasContext.beginPath();
-canvasContext.rect(apple, 400, 25, 25);
-canvasContext.fillStyle = 'red';
-canvasContext.fill();
-
-function move(e){
-    if(e.keycode = 38){
-        snakePositionY += -5;
-    }
-    if(e.keyCode = 40){
-        snakePositionY += 5;
