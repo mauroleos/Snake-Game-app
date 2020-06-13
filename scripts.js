e2f85e6fcf3147d123bca61afdacbd37575d3490
mauro Leos <mauroleos94@gmail.com>
2020-06-13 13:48:21 -0500

    working on random apple placement

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-const DEBUG = false; 
+const DEBUG = true;
 var canvas = document.getElementById('game-canvas');
 var canvasContext = canvas.getContext('2d');
 document.addEventListener('keydown', keyPush);
 
 var snakeVelocityX = 0;
-var snakeVelocityY = 0; 
+var snakeVelocityY = 0;
 var snakePositionX = 150;
 var snakePositionY = 150;
 var appleX = 400;
@@ -16,15 +16,18 @@ if (DEBUG === true) {
     appleX = 200;
     appleY = 150;
 
-    gameTimeInterval = 1000;
+    gameTimeInterval = 100;
 }
 
 setInterval(game, gameTimeInterval);
 
 function game() {
- snakePositionX += snakeVelocityX;
- snakePositionY += snakeVelocityY;
-    
+    snakePositionX += snakeVelocityX;
+    snakePositionY += snakeVelocityY;
+
+    // var appleX = 400;
+    // var appleY = 300;
+
     canvasContext.beginPath();
     canvasContext.fillStyle = 'black';
     canvasContext.rect(0, 0, canvas.width, canvas.height);
@@ -46,25 +49,31 @@ function game() {
     if (snakePositionY < 0 || snakePositionY >= canvas.height) {
         alert('Game Over');
     }
+
+    if (snakePositionX > appleX && snakePositionX < parseInt(appleX) + 15 &&
+        snakePositionY > appleY && snakePositionY < parseInt(appleY) + 15
+    ) {
+        alert('apple has been consumed');
+    }
 }
 
-function keyPush(e){
-    switch(e.keyCode) {
-        case 37 : 
-        snakeVelocityX = -5;
-        snakeVelocityY = 0;
-        break;
-        case 38 : 
-        snakeVelocityX = 0;
-        snakeVelocityY = -5;
-        break;
-        case 39 : 
-        snakeVelocityX = 5;
-        snakeVelocityY = 0;
-        break;
-        case 40 : 
-        snakeVelocityX = 0;
-        snakeVelocityY = 5;
-        break;
+function keyPush(e) {
+    switch (e.keyCode) {
+        case 37:
+            snakeVelocityX = -5;
+            snakeVelocityY = 0;
+            break;
+        case 38:
+            snakeVelocityX = 0;
+            snakeVelocityY = -5;
+            break;
+        case 39:
+            snakeVelocityX = 5;
+            snakeVelocityY = 0;
+            break;
+        case 40:
+            snakeVelocityX = 0;
+            snakeVelocityY = 5;
+            break;
     }
 }
