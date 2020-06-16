9e603c52aa47dc36210ea26b7d254d65998b4563
mauro Leos <mauroleos94@gmail.com>
2020-06-16 08:57:28 -0500

    Worked on JS

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // const DEBUG = true;
 var canvas = document.getElementById('game-canvas');
 var canvasContext = canvas.getContext('2d');
-// document.addEventListener('keydown', keyPush);
+document.addEventListener('keydown', keyPush);
 
 var appleX = 400;
 var appleY = 300;
@@ -16,29 +16,47 @@ function Snake() {
     this.xSnakeVelocity = square;
     this.ySnakeVelocity = square;
 
-    this.draw = function snakeFeatures() {
+    this.draw = function () {
         canvasContext.fillStyle = 'yellow';
         canvasContext.fillRect(this.x, this.y, square, square);
 
-        this.drawNewSnake = function() {
+        this.drawNewSnake = function () {
             this.x += this.xSnakeVelocity;
             this.y += this.ySnakeVelocity;
         }
     }
+};
+function keyPush(e) {
+    switch (e.keyCode) {
+        case 37:
+            this.xSnakeVelocity = -5;
+            this.ySnakeVelocity = 0;
+            break;
+        case 38:
+            this.xSnakeVelocity = 0;
+            this.ySnakeVelocity = -5;
+            break;
+        case 39:
+            this.xSnakeVelocity = 5;
+            this.ySnakeVelocity = 0;
+            break;
+        case 40:
+            this.xSnakeVelocity = 0;
+            this.ySnakeVelocity = 5;
+            break;
+    }
 }
 
-function drawSnake(){
+function drawSnake() {
     snake = new Snake();
-    snake.draw();
 };
 drawSnake();
-
-setInterval(game, 100);
 
 function game() {
     snake.draw();
     snake.drawNewSnake();
 };
+setInterval(game, 100);
 
 // var gameTimeInterval = 40;
 
@@ -51,36 +69,10 @@ function game() {
 
 // var game = setInterval(game, gameTimeInterval);
 
-//     canvasContext.beginPath();
-//     canvasContext.fillStyle = 'red';
-//     canvasContext.rect(appleX, appleY, 15, 15);
-//     canvasContext.fill();
-
 //     if (snakePositionX < 0 || snakePositionX >= canvas.width) {
 //         alert('Game Over');
 //     }
 //     if (snakePositionY < 0 || snakePositionY >= canvas.height) {
 //         alert('Game Over');
-//     }
-// }
-
-// function keyPush(e) {
-//     switch (e.keyCode) {
-//         case 37:
-//             snakeVelocityX = -5;
-//             snakeVelocityY = 0;
-//             break;
-//         case 38:
-//             snakeVelocityX = 0;
-//             snakeVelocityY = -5;
-//             break;
-//         case 39:
-//             snakeVelocityX = 5;
-//             snakeVelocityY = 0;
-//             break;
-//         case 40:
-//             snakeVelocityX = 0;
-//             snakeVelocityY = 5;
-//             break;
 //     }
 // }
