7f96c75e06ff528400bcc8be4af2d42cf475262d
mauro Leos <mauroleos94@gmail.com>
2020-06-16 23:39:30 -0500

    Added dnake moblility and border detection

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 // const DEBUG = true;
 var canvas = document.getElementById('game-canvas');
 var canvasContext = canvas.getContext('2d');
-document.addEventListener('keydown', keyPush);
 
 var appleX = 400;
 var appleY = 300;
@@ -11,41 +10,49 @@ const rows = canvas.height / square;
 const columns = canvas.width / square;
 
 function Snake() {
-    this.x = 200;
-    this.y = 150;
+    this.x = 0;
+    this.y = 0;
     this.xSnakeVelocity = square;
-    this.ySnakeVelocity = square;
+    this.ySnakeVelocity = 0;
 
     this.draw = function () {
         canvasContext.fillStyle = 'yellow';
         canvasContext.fillRect(this.x, this.y, square, square);
+    }
 
-        this.drawNewSnake = function () {
-            this.x += this.xSnakeVelocity;
-            this.y += this.ySnakeVelocity;
+    this.drawNewSnake = function () {
+        this.x += this.xSnakeVelocity;
+        this.y += this.ySnakeVelocity;
+
+        if (parseInt(this.x) + 15 > canvas.width || this.x < 0) {
+            alert('Game Over');
+        }
+        if (parseInt(this.y) + 15 > canvas.height || this.y < 0) {
+            alert('Game Over');
+        }
+    };
+
+    this.snakeDirection = function (keyPressed) {
+        switch (keyPressed) {
+            case 37:
+                this.xSnakeVelocity = -square;
+                this.ySnakeVelocity = 0;
+                break;
+            case 38:
+                this.xSnakeVelocity = 0;
+                this.ySnakeVelocity = -square;
+                break;
+            case 39:
+                this.xSnakeVelocity = square;
+                this.ySnakeVelocity = 0;
+                break;
+            case 40:
+                this.xSnakeVelocity = 0;
+                this.ySnakeVelocity = square;
+                break;
         }
     }
 };
-function keyPush(e) {
-    switch (e.keyCode) {
-        case 37:
-            this.xSnakeVelocity = -5;
-            this.ySnakeVelocity = 0;
-            break;
-        case 38:
-            this.xSnakeVelocity = 0;
-            this.ySnakeVelocity = -5;
-            break;
-        case 39:
-            this.xSnakeVelocity = 5;
-            this.ySnakeVelocity = 0;
-            break;
-        case 40:
-            this.xSnakeVelocity = 0;
-            this.ySnakeVelocity = 5;
-            break;
-    }
-}
 
 function drawSnake() {
     snake = new Snake();
@@ -53,10 +60,16 @@ function drawSnake() {
 drawSnake();
 
 function game() {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
     snake.draw();
     snake.drawNewSnake();
 };
 setInterval(game, 100);
+
+document.addEventListener('keydown', function keyPush(e) {
+    const keyPressed = e.keyCode;
+    snake.snakeDirection(keyPressed);
+});
 
 // var gameTimeInterval = 40;
 
@@ -68,11 +81,3 @@ setInterval(game, 100);
 // }
 
 // var game = setInterval(game, gameTimeInterval);
-
-//     if (snakePositionX < 0 || snakePositionX >= canvas.width) {
-//         alert('Game Over');
-//     }
-//     if (snakePositionY < 0 || snakePositionY >= canvas.height) {
-//         alert('Game Over');
-//     }
-// }
